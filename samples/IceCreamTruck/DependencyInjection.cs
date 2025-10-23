e9933ff40cee513999ea5d381798496306806fbe
Florian Pirard <florian.pirard@lotea.be>
2025-10-23 18:40:22 +0200

    feat: Implement Ice Cream Truck Web API with order management and Swagger integration

--- samples/IceCreamTruck/DependencyInjection.cs
+++ samples/IceCreamTruck/DependencyInjection.cs
@@ -2,12 +2,14 @@ using IceCreamTruck.Repositories;
 
 using Microsoft.Extensions.DependencyInjection;
 
+namespace IceCreamTruck;
+
 public static class DependencyInjection
 {
-    public static IServiceCollection AddIceCreamTruckServices(this IServiceCollection services)
+    public static IServiceCollection AddIceCreamTruck(this IServiceCollection services)
     {
+        services.AddMiddleManZero();
         services.AddSingleton<IOrderRepository, OrderRepository>();
-
         return services;
     }
 }
